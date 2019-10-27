163e29cf74a4cae9853e10005a2f088337fd5bf2
Pablo <pmotta.dev@gmail.com>
2019-10-27 16:14:01 -0700

    database schema and seeds

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+-- Create the burgers_db.
+-- Switch to or use the burgers_db.
+-- Create a burgers table with these fields:
+-- id: an auto incrementing int that serves as the primary key.
+-- burger_name: a string.
+-- devoured: a boolean.
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Plain Burger');
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name, sleepy) VALUES ('Double Cheese', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Bacon Burger', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Umami Burger', true);
+INSERT INTO burgers (name) VALUES ('Cowboy Burger');
