4bcd2eb4061c451230040409e9d3cc7b30c6c557
Pablo <pmotta.dev@gmail.com>
2019-10-29 22:55:06 -0700

    added readme.md

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,23 @@
 // Set up MySQL connection.
-var mysql = require('mysql');
+const mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'root',
-    database: 'burgers_db'
-});
+let connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'root',
+        database: 'burgers_db'
+    });
+}
 
 // we create a promise so that we can make sure any request waits
 // for connection to be connected before running any command.
 const connectionPromise = new Promise((resolve, reject) => {
-    connection.connect(function(err) {
+    connection.connect(err => {
         if (err) {
             return reject(err);
         }
